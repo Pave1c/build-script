7e72a856228ef7fbb55d15fca26921075e083fda
Pave1c <dimzzy23@gmail.com>
2024-06-18 07:11:23 +0900

    first commit

--- setup.sh
+++ setup.sh
@@ -2,3 +2,8 @@
 
 #dependency 
 sudo apt update -y && sudo apt upgrade -y && sudo apt install nano bc bison ca-certificates curl flex gcc git libc6-dev libssl-dev openssl python-is-python3 ssh wget zip zstd sudo make clang gcc-arm-linux-gnueabi software-properties-common build-essential libarchive-tools gcc-aarch64-linux-gnu -y && sudo apt install build-essential -y && sudo apt install libssl-dev libffi-dev libncurses5-dev zlib1g zlib1g-dev libreadline-dev libbz2-dev libsqlite3-dev make gcc -y && sudo apt install pigz -y && sudo apt install python2 -y && sudo apt install python3 -y && sudo apt install cpio -y && sudo apt install lld -y
+
+
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
